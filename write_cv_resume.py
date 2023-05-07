bd2639870a89c8b2b11bc15c21f01d41fd1c8936
John Cheng <john.z.cheng@gmail.com>
2023-05-07 11:13:09 -0400

    change file naming convention

--- write_cv_resume.py
+++ write_cv_resume.py
@@ -3,13 +3,13 @@ import sys
 import json
 from jinja2 import Environment, FileSystemLoader
 
-prefix = "anon"
+root = "anon"
 try:
-    prefix = sys.argv[1]
+    root = sys.argv[1]
 except:
     pass
 
-with open(file=prefix + "_resume_schema.json", mode="r") as data_file:
+with open(file="data_for_" + root + ".json", mode="r") as data_file:
     resume_data = json.load(data_file)
 
 # print (resume_data)
@@ -18,7 +18,7 @@ template = environment.get_template("cv_resume.txt.jinja")
 
 content = template.render(resume_data)
 
-filename = prefix + "_resume.html"
+filename = root + "_resume.html"
 with open(filename, mode="w", encoding="utf-8") as message:
     environment.parse(template)
     message.write(content)
