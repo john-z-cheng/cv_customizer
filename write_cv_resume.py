e09792d8b7bfdde1a7964496595c82fe5d9958f0
John Cheng <john.z.cheng@gmail.com>
2023-05-08 16:56:26 -0400

    add code to read keywords from file

--- write_cv_resume.py
+++ write_cv_resume.py
@@ -12,6 +12,18 @@ except:
 with open(file="data_for_" + root + ".json", mode="r") as data_file:
     resume_data = json.load(data_file)
 
+# keywords_list contains a keyword (or keyword phrase) on each line
+keywords = []
+try:
+    with open(file="keywords_for_" + root + ".txt", mode="r") as keyword_file:
+        for line in keyword_file:
+            if len(line.strip()): keywords.append(line.strip())
+except (OSError):
+    pass
+print ("Keywords to be emphasized:", keywords)
+
+# TODO: add bold to keywords in resume_data
+
 # print (resume_data)
 environment = Environment(loader=FileSystemLoader("."))
 template = environment.get_template("cv_resume.txt.jinja")
